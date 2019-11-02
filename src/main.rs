62f871d04c50b63ba39071172cd1a1668efaf03b
Neal Fennimore <hi@neal.codes>
2019-11-01 21:43:40 -0400

    Add in custom panic handler

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 #![no_std]
 
+// Add in core panic
+use core::panic::PanicInfo;
+
+// This function is called on panic
+// If the main thread panics it will terminate all your threads and end your program with code 101.
+// Otherwise panic is handled by a single thread if the panic! method is used
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! { // The PanicInfo parameter contains the file and line where the panic happened and the optional panic message.
+    // The function should never return, so it is marked as a diverging function by returning the “never” type !.
+
+    // We can't do anything with it yet, so just loop
+    loop {}
+}
+
 fn main() {
-    println!("Hello, world!");
 }
